bf31c199572d17b6ebb8387128c2276d043b03df
Brian Cardarella <bcardarella@gmail.com>
2024-11-17 14:21:39 -0500

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Animate.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @scm_url "https://github.com/dockyard/animate"
+
   def project do
     [
       app: :animate,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      description: description(),
+      source_url: @scm_url,
     ]
   end
 
@@ -18,12 +25,34 @@ defmodule Animate.MixProject do
     ]
   end
 
+  def description(), do: "Animation keyframe generator for Scenic"
+
+  defp package do
+    [
+      maintainers: ["Brian Cardarella"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @scm_url},
+      files:
+        ~w(lib CHANGELOG.md LICENSE.md mix.exs README.md .formatter.exs)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_ref: "v#{@version}",
+      assets: "assets/",
+      extras: ["README.md"]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:easing, "~> 0.3.1"},
       {:easing, path: "../easing"},
-      {:scenic, "~> 0.11.0-beta.0"}
+      {:scenic, "~> 0.11.0-beta.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
